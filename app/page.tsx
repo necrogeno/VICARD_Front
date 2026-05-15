4cd4019626de32537dd28620e3c801b80586077c
nerogeno <necrogeno302@gmail.com>
2026-05-15 15:15:11 -0600

    cambios leves

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,11 @@ export default function Home() {
         <h2>Apellidos:</h2>
         <h2>Puestos de trabajo:</h2>
         <h2>Direccion:</h2>
+        <h2>Telefono:</h2>
+        <h2>Email:</h2>
+        <h2>Colonia:</h2>
+        <h2>Unidad Aministrativa:</h2>
+        <h2>Activo:</h2>
       </div>
     </div>
   );
